90a9202bf941e0a0815fcf7bd6a58baccff44f4f
Christoffer Rod Greffel <33146387+Suduko123@users.noreply.github.com>
2023-02-28 11:44:10 +0100

    Upload

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 //Bruger .querySelectorAll til at selecte alle classes med det navn på hele siden
 const dageCirkel = document.querySelectorAll(".dage-cirkel");
 
-//Vi bruger .forEach for at ramme hver cirkel 
+//Vi bruger .forEach for at ramme hver cirkel
 
-dageCirkel.forEach(element => {  
-    // ()=> {} her tilføjer vi en helt almindelig function
-    element.addEventListener("click", () => {
-        // element.style.backgroundColor = "#26245E" erstattes med nedenstående, da vi referer til en class i CSS der aktiveres i nedenstående linje
-        //classList er den måde man får adgang til classes på så man kan skrive fx toggle. 
-        element.classList.toggle("color-change");
-    })
+dageCirkel.forEach((element) => {
+  // ()=> {} her tilføjer vi en helt almindelig function
+  element.addEventListener("click", () => {
+    // element.style.backgroundColor = "#26245E" erstattes med nedenstående, da vi referer til en class i CSS der aktiveres i nedenstående linje
+    //classList er den måde man får adgang til classes på så man kan skrive fx toggle.
+    element.classList.toggle("color-change");
+  });
 });
-
 
 const checkCirkel = document.querySelectorAll("check-cirkel");
 const checkPil = document.querySelectorAll(".fa-solid fa-check");
 
-
-checkCirkel.forEach(element => {  
-    element.addEventListener("click", () => {
-    
+checkCirkel.forEach((element) => {
+  element.addEventListener("click", () => {
     element.classList.toggle("check-show");
-
-    })
+  });
 });
 
+// Hide header on scroll
 
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.querySelector("header").style.top = "0";
+//   } else {
+//     document.querySelector("header").style.top = "-150px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
